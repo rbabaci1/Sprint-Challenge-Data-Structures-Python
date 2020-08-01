b295d83738fae4b1cdbc8bd47a67a17087ca51a5
rabah babaci <4rabah@gmail.com>
2020-08-01 10:18:19 -0700

    finish the ring_buffer implementation

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,26 +1,3 @@
-# class Node:
-#     def __init__(self, value, next=None):
-#         self.value = value
-#         self.next = next
-
-
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
-#         self.tail = None
-#         self.length = 0
-
-#     def add_to_tail(self, value):  # O(1)
-#         new_node = Node(value)
-#         if self.tail:
-#             self.tail.next = new_node
-#             self.tail = new_node
-#         else:
-#             self.head = new_node
-#             self.tail = new_node
-#         self.length += 1
-
-
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
