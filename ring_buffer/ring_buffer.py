a5fd471e5d6ec6b6b27d212552d54171eb328b6b
rabah babaci <4rabah@gmail.com>
2020-08-03 19:32:27 -0700

    add ring_buffer linked list version

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,3 +1,37 @@
+# class Node:
+#     def __init__(self, value, next=None):
+#         self.value = value
+#         self.next = next
+
+
+# class LinkedList:
+#     def __init__(self):
+#         self.head = self.tail = None
+#         self.length = 0
+
+#     def add_to_tail(self, value):
+#         node = Node(value)
+#         if not self.head:
+#             self.head = node
+#             self.tail = node
+#         else:
+#             self.tail.next = node
+#             self.tail = node
+#         self.length += 1
+
+
+# class RingBuffer:
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.storage = LinkedList()
+
+#     def append(self, item):
+#         if self.storage.length < self.capacity:
+#             self.storage.add_to_tail(item)
+#         else:
+#             pass
+
+
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -10,8 +44,7 @@ class RingBuffer:
         else:
             if self.index == len(self.storage):
                 self.index = 0
-            self.storage.pop(self.index)
-            self.storage.insert(self.index, item)
+            self.storage[self.index] = item
         self.index += 1
 
     def get(self):
